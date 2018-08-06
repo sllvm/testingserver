998f79f6d852d83519a9f6b1b325dcef7b264d7c
sllvm <40460343+sllvm@users.noreply.github.com>
2018-08-06 20:31:17 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+var myImage = document.querySelector('img');
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myImage.onclick = function() {
+	setUserName();
+}
